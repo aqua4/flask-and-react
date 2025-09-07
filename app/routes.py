10f25cb4689e093113173dc00939154cf3927010
aqua4 <kitamrofni@gmail.com>
2025-09-07 16:13:07 +0200

    add comments to routes

--- app/routes.py
+++ app/routes.py
@@ -10,11 +10,13 @@ def index():
     return render_template('index.html')
 
 
+# service-worker.js
 @app.route('/service-worker.js', methods=['GET'])
 def serve_service_worker():
     return send_from_directory(app.static_folder + '/..', 'service-worker.js')
 
 
+# add_value
 @app.route('/add_value', methods=['POST'])
 def add_value():
     if Value.add_value(request.get_json().get('value')):
@@ -22,6 +24,7 @@ def add_value():
     return jsonify(status='bad request')
 
 
+# get_values
 @app.route('/get_values', methods=['GET'])
 def get_values():
     return jsonify(values=[value.serialize() for value in Value.get_values()])
